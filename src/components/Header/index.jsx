b4c1fdac24ee1812670aeca9d01f97ccc461f096
Bruno Rodrigues <brunohrsilva1@gmail.com>
2020-10-08 15:23:03 -0300

    Add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Header() {
+	return (
+		<div className="header-content">
+			<h1>Last posts</h1>
+			<i className="fas fa-chevron-down"></i>
+		</div>
+	);
+}
