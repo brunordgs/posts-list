f30dd1d8fa2ca968d0654ebd07e41bd28fa1263b
Bruno Rodrigues <brunohrsilva1@gmail.com>
2020-10-08 15:23:51 -0300

    Add Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Footer() {
+	return (
+		<p className="footer">
+			<strong>
+				&lt;Made with <i className="fas fa-heart"></i> &#47;&gt;
+			</strong>
+		</p>
+	);
+}
